30c22fcc075cb0dc09272430eb232ea726de8535
functionalname <tanishqnaik130@gmail.com>
2026-02-13 12:09:27 +0530

    this is test push

--- Java with DSA/functions/funCir.java
+++ Java with DSA/functions/funCir.java
@@ -1,13 +1,6 @@
-import java.util.*;
 
-class funCir {
-    public static void printCirp(double n){
-        double cir=2*3.14*n;
-        System.out.println(cir);
-    }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        double n= sc.nextFloat();
-        printCirp(n);
-    }
+class funCir{
+public static void main(String args[]){
+
+}
 }
---
+++ JavaDSA/Basic syntax/functionalfirst.java
@@ -0,0 +1,9 @@
+
+class functionalfirst{
+    public static void main(String args[]){
+        System.out.println("Tanishq Naik");
+        System.out.println("Parul University Goa");
+        System.out.println("Canacona Goa");
+        System.out.println("St.Anthony High school");
+    }
+}
